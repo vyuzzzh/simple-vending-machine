0705edd7cf6af39bedaedf49f1d725e89b55bb99
MishaVyuzh <vyuzzzh@yandex.ru>
2020-04-03 21:27:28 +0300

    Updates

---
+++ precache-manifest.e31ae44b05ed90a3e46697c33a99daa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65da089c55e9147748ca9ff08a4336df",
+    "url": "/simple-vending-machine/index.html"
+  },
+  {
+    "revision": "e0eae211f4037f2db255",
+    "url": "/simple-vending-machine/static/css/main.4a1af7fb.chunk.css"
+  },
+  {
+    "revision": "1d69e2cd389511900cef",
+    "url": "/simple-vending-machine/static/js/2.cb56d128.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-vending-machine/static/js/2.cb56d128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0eae211f4037f2db255",
+    "url": "/simple-vending-machine/static/js/main.e3fe6c7b.chunk.js"
+  },
+  {
+    "revision": "af482e16bf2396ddbabf",
+    "url": "/simple-vending-machine/static/js/runtime-main.fa328485.js"
+  }
+]);
